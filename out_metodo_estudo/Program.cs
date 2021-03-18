e8f63d15b6911195c03d915151c56625e904ce59
Ricardo Mubarak <psycho696@gmail.com>
2021-03-18 19:14:52 -0300

    Adicionar arquivos de projeto.

---
+++ out_metodo_estudo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace out_metodo_estudo
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int d1, d2, quoc, rest;
+            Console.WriteLine("digite o dividendo");
+            d1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("digite o divisor");
+            d2 = int.Parse(Console.ReadLine());
+            quoc = Divisao(d1, d2, out rest);
+            Console.WriteLine($"{d1}/{d2} tem quociente {quoc} e resto {rest}");
+            Console.ReadLine();
+        }
+
+       static int Divisao(int dividendo, int divisor, out int resto)
+        {
+            int quociente;
+            quociente = dividendo / divisor;
+            resto = dividendo % divisor;
+            return quociente;
+        }
+    }
+}
